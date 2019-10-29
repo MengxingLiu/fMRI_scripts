3c3c877f09922f5c227a5630dc6f023eeb9bbbfa
Liu Mengxing <mengluo1990@gmail.com>
2019-10-29 11:59:57 +0100

    Update dwipreprocess.sh

--- dwipreprocess.sh
+++ dwipreprocess.sh
@@ -6,5 +6,10 @@
 cd rawdata
 mkdir sub-02
 cd sub-02
-mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*64/ sub-02.mif
+mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*64/ sub-02_dwi.mif
+mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*SBRef_63/ sub-02_SBRef.mif
+mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*b0*62/ sub-02_b0.mif
+mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*b0*61/ sub-02_b0SBRef.mif
+
+
 
