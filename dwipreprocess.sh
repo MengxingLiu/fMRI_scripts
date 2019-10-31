92223fa1f392698772986f557815351379e9657e
Liu Mengxing <mengluo1990@gmail.com>
2019-10-31 16:48:30 +0100

    Update dwipreprocess.sh

--- dwipreprocess.sh
+++ dwipreprocess.sh
@@ -11,5 +11,18 @@ mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*SBRef_63/ sub-02_SBRef.mif
 mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*b0*62/ sub-02_b0.mif
 mrconvert ../../sourcedata/sub02/DWI/diff_cmrr*b0*61/ sub-02_b0SBRef.mif
 
+## Dilated brain mask can offer a significant speed improvement
+dwi2mask ../raw/dwi.mif - | maskfilter - dilate preproc_mask.mif -npass 5
+dwidenoise ../raw/dwi.mif denoise.mif -noise noiselevel.mif -mask preproc_mask.mif
+
+# Gibbs ringing correction
+mrdegibbs denoise.mif degibbs.mif
+
+## pair b0 AP and PA
+dwiextract -bzero sub-02_dwi.mif - | mrmath - -axis 3 mean sub-02_b0_AP.mif
+mrcat sub-02_b0.mif sub-02_b0_AP.mif b0_pair.mif
+## Motion & distortion correction
+dwifslpreproc degibbs.mif geomcorr.mif -pe_dir AP -rpe_pair -se_epi
+b0pair.mif
 
 
